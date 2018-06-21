f57164349b374ad758c50d96f08de408e4a070a3
xeliot <daveho8888@gmail.com>
2018-06-20 22:11:34 -0700

    create swift project

---
+++ ThoughtJar/ThoughtJar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThoughtJar
+//
+//  Created by Dave Ho on 6/20/18.
+//  Copyright © 2018 Thought Jar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
