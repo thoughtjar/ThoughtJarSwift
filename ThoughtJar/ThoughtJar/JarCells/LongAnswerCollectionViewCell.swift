d610bfc4499f56f867e8d570f4a73f24b3a83f6c
xeliot <daveho8888@gmail.com>
2018-06-26 11:18:06 -0700

    create long answer cell files

---
+++ ThoughtJar/ThoughtJar/JarCells/LongAnswerCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  LongAnswerCollectionViewCell.swift
+//  ThoughtJar
+//
+//  Created by Dave Ho on 6/26/18.
+//  Copyright © 2018 Thought Jar. All rights reserved.
+//
+
+import UIKit
+
+class LongAnswerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
